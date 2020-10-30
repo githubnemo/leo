1f1b3468d7145f2a659a5c05cc431433cccbc0cf
Marian Tietz <marian.tietz@ottogroup.com>
2020-10-30 19:51:55 +0100

    Spanish support (draft)

--- main.go
+++ main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"flag"
-	"log"
 	"fmt"
-	"strings"
 	"github.com/PuerkitoBio/goquery"
+	"log"
 	"net/url"
+	"strings"
 )
 
 func min(a, b int) int {
@@ -27,18 +27,18 @@ func main() {
 		log.Fatal("Word missing.")
 	}
 
-	doc, err := goquery.NewDocument("http://dict.leo.org/englisch-deutsch/" + url.QueryEscape(flag.Arg(0)))
+	doc, err := goquery.NewDocument("http://dict.leo.org/spanisch-deutsch/" + url.QueryEscape(flag.Arg(0)))
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	sections := []struct{ header, parent, en, de string }{
-		{"Substantive", "div[data-dz-name=subst]", "tr [lang=en]", "tr [lang=de]"},
-		{"Verben", "div[data-dz-name=verb]", "tr [lang=en]", "tr [lang=de]"},
-		{"Adj./Adv.", "div[data-dz-name=adjadv]", "tr [lang=en]", "tr [lang=de]"},
-		{"Phrasen", "div[data-dz-name=phrase]", "tr [lang=en]", "tr [lang=de]"},
-		{"Beispiele", "div[data-dz-name=example]", "tr [lang=en]", "tr [lang=de]"},
+		{"Substantive", "div[data-dz-name=subst]", "tr [lang=es]", "tr [lang=de]"},
+		{"Verben", "div[data-dz-name=verb]", "tr [lang=es]", "tr [lang=de]"},
+		{"Adj./Adv.", "div[data-dz-name=adjadv]", "tr [lang=es]", "tr [lang=de]"},
+		{"Phrasen", "div[data-dz-name=phrase]", "tr [lang=es]", "tr [lang=de]"},
+		{"Beispiele", "div[data-dz-name=example]", "tr [lang=es]", "tr [lang=de]"},
 	}
 
 	for _, section := range sections {
@@ -53,7 +53,7 @@ func main() {
 		en := parent.Find(section.en)
 		de := parent.Find(section.de)
 
-		for i := 0; i < min(en.Length(),de.Length()); i++ {
+		for i := 0; i < min(en.Length(), de.Length()); i++ {
 			enText := sanitize(en.Eq(i).Text())
 			deText := sanitize(de.Eq(i).Text())
 			fmt.Printf("| %30s | %30s |\n", enText, deText)
